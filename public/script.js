9a871c5551fae57b831d41262c601242cf68d81e
astefanski1 <aleksander.stefanski@outlook.com>
2018-01-13 22:49:21 +0100

    added second site, improved code

--- public/script.js
+++ public/script.js
@@ -1,13 +1,34 @@
 $(document).ready(function () {
+    getCategories();
+    checkForm();
+
+    $('#site').on('change', function () {
+        $('#type')
+            .find('option')
+            .remove()
+            .end();
+        getCategories();
+        checkForm()
+    });
+
+    function checkForm() {
+        if ($('#site').val() === 'https://pl.wikipedia.org/wiki/Portal:Kategorie_G%C5%82%C3%B3wne') {
+            $('#articleLength').prop('disabled', true);
+        } else {
+            $('#articleLength').prop('disabled', true);
+        }
+        $('#searchArticles').attr('disabled', 'disabled');
+    }
+
+    $('#articlesNumber').on('keyup', function () {
+        let inputValue = parseInt($('#articlesNumber').val());
+        if (inputValue <= 0 || inputValue > 999 || $('#articlesNumber').val() === '') {
+            $('#searchArticles').attr('disabled', 'disabled');
+        } else $('#searchArticles').removeAttr('disabled');
+    });
+
     $('#getCategories').on('click', function () {
-        $.get("/getCategories", function (data) {
-            data.forEach(link => {
-                $('#type').append($('<option>', {
-                    value: link.value,
-                    text: link.text
-                }));
-            });
-        })
+        getCategories();
     });
 
     $('#searchArticles').on('click', function () {
@@ -20,7 +41,23 @@ $(document).ready(function () {
             url: "/download",
             data: {site: site, type: type, articlesNumber: articlesNumber, articleLength: articleLength}
         }).done(function () {
-            console.log("Send form");
+            console.log('Senden request to download articles!');
         });
     });
+
+    function getCategories() {
+        let categoryURL = $('select#site').val();
+        $.ajax({
+            method: "POST",
+            url: "/getCategories",
+            data: {site: categoryURL}
+        }).done(function (data) {
+            data.forEach(link => {
+                $('#type').append($('<option>', {
+                    value: link.value,
+                    text: link.text
+                }));
+            });
+        });
+    }
 });
--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+const BI_ARTICLES_NUMBER_ON_PAGE = 50;
+
 let fs = require('fs');
 let path = require('path');
 let request = require('request');
@@ -15,28 +17,42 @@ app.listen(4200, () => {
 });
 
 app.get('/', function (req, res) {
-
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/getCategories', function (req, res) {
-    let url = 'https://pl.wikipedia.org/wiki/Portal:Kategorie_G%C5%82%C3%B3wne';
+let buisnessInsiderCategoryLinks = [
+    {value: 'https://businessinsider.com.pl/firmy', text: 'Firmy'},
+    {value: 'https://businessinsider.com.pl/finanse', text: 'Finanse'},
+    {value: 'https://businessinsider.com.pl/twoje-pieniadze', text: 'Twoje Pieniądze'},
+    {value: 'https://businessinsider.com.pl/motoryzacja', text: 'Motoryzacja'},
+    {value: 'https://businessinsider.com.pl/technologie', text: 'Technologie'},
+    {value: 'https://businessinsider.com.pl/rozwoj-osobisty', text: 'Rozwój osobisty'},
+    {value: 'https://businessinsider.com.pl/lifestyle', text: 'Lifestyle'},
+    {value: 'https://businessinsider.com.pl/media', text: 'Media'},
+];
+
+app.post('/getCategories', function (req, res) {
+    let url = req.body.site;
     request(url, (err, response, body) => {
         let category_links = [];
         let isCategory = true;
         let $ = cheerio.load(body);
-        $('td p a', 'div#bodyContent').each(function () {
-            if ($(this).text() === 'Jak działają kategorie na Wikipedii?') isCategory = false;
-            if (isCategory) {
-                category_links.push({
-                    value: 'https://pl.wikipedia.org/wiki/Kategoria:' + $(this).text(),
-                    text: $(this).text()
-                });
-            }
-        });
+        console.log(url);
+        if (url === 'https://pl.wikipedia.org/wiki/Portal:Kategorie_G%C5%82%C3%B3wne') {
+            $('td p a', 'div#bodyContent').each(function () {
+                if ($(this).text() === 'Jak działają kategorie na Wikipedii?') isCategory = false;
+                if (isCategory) {
+                    category_links.push({
+                        value: 'https://pl.wikipedia.org/wiki/Kategoria:' + $(this).text(),
+                        text: $(this).text()
+                    });
+                }
+            });
+        } else if (url === 'https://businessinsider.com.pl/') {
+            category_links = buisnessInsiderCategoryLinks;
+        }
         res.send(category_links);
     });
-
 });
 
 app.post('/download', (req, res) => {
@@ -45,7 +61,6 @@ app.post('/download', (req, res) => {
 });
 
 function getFormData(data) {
-    console.log("Form data passed by user: ");
     console.log(data);
     let site = data.site;
     let type = data.type;
@@ -53,8 +68,34 @@ function getFormData(data) {
     let articlesNumber = data.articlesNumber;
 
     switch (site) {
-        case 'wikipedia':
-            getWebPageDataWikipedia(type, maxLength, articlesNumber, site);
+        case 'https://pl.wikipedia.org/wiki/Portal:Kategorie_G%C5%82%C3%B3wne':
+            getWebPageDataWikipedia(type, maxLength, articlesNumber, 'wikipedia');
+            break;
+        case 'https://businessinsider.com.pl/':
+            getWebPageDataBuisnessInsider(type, maxLength, articlesNumber, 'buisness-insider');
+            break;
+    }
+}
+
+function getWebPageDataBuisnessInsider(url, maxLength, articlesNumber, site) {
+    let links = [];
+    let page;
+    let pagesToDownload = Math.ceil(articlesNumber / BI_ARTICLES_NUMBER_ON_PAGE);
+    let linksLength = pagesToDownload * BI_ARTICLES_NUMBER_ON_PAGE;
+    for (let i = 1; i <= pagesToDownload; i++) {
+        page = url + '?s=' + i;
+        request(page, (err, response, body) => {
+            if (!err && response.statusCode === 200) {
+                let $ = cheerio.load(body);
+                $('article h2 a', 'div.river').each(function () {
+                    links.push($(this).attr('href'));
+                });
+            }
+            if (links.length === linksLength) {
+                getArticles(links, maxLength, articlesNumber, site);
+            }
+        });
+        if (i === pagesToDownload) isDone = false;
     }
 }
 
@@ -74,24 +115,46 @@ function getWebPageDataWikipedia(url, maxLength, articlesNumber, site) {
 function getArticles(links, maxLength, articlesNumber, site) {
     if (articlesNumber !== undefined && articlesNumber > 0) {
         for (let index = 0; index < articlesNumber; index++) {
-            saveArticle(links[index], index, site);
+            getArticleData(links[index], index, site);
         }
     } else console.log("No articles number passed");
 }
 
-function saveArticle(url, articleNumber, site) {
-    request(url, (err, response, body) => {
-        if (!err && response.statusCode === 200) {
-            let $ = cheerio.load(body);
-            let article = " ";
-            $('p', 'div#mw-content-text').each(function () {
-                article += $(this).text();
-            });
-            console.log("Saving article from wikipedia.org");
-            let file = __dirname + '/articles/' + site + '/' + articleNumber + '.txt';
-            fs.writeFileSync(file, article);
-        }
-    });
+function getArticleData(url, articleNumber, site) {
+    if (site === 'wikipedia') {
+        request(url, (err, response, body) => {
+            if (!err && response.statusCode === 200) {
+                let $ = cheerio.load(body);
+                let article = " ";
+                $('p', 'div#mw-content-text').each(function () {
+                    article += $(this).text();
+                });
+                saveArticle(site, articleNumber, article);
+            }
+        });
+    }
+    if (site === 'buisness-insider') {
+        request(url, (err, response, body) => {
+            if (!err && response.statusCode === 200) {
+                let $ = cheerio.load(body);
+                let article = " ";
+                $('p', 'div#content').each(function () {
+                    article += $(this).text();
+                });
+                saveArticle(site, articleNumber, article);
+            }
+        });
+    }
+}
+
+function getRandomKey() {
+    return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+}
+
+function saveArticle(site, articleNumber, article) {
+    console.log("Saving article from " + site);
+    let file = __dirname + '/articles/' + site + '/' + articleNumber + "_" + getRandomKey() + '.txt';
+    fs.writeFileSync(file, article);
 }
 
 exports = module.exports = app;
